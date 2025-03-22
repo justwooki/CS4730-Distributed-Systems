ca233fc44e64001a0970b06b5b5c114560912e55
justwooki <79425101+justwooki@users.noreply.github.com>
2025-03-22 13:37:50 -0400

    prj4 - moved to intellij build bc gradle was being weird; setup proposers and acceptors

---
+++ Project4-Paxos/src/main/java/ProcessInfo.java
@@ -0,0 +1,36 @@
+package main.java;
+
+/**
+ * Represents the information by which to identify a process by. Includes specifically the unique
+ * id and host name of the process.
+ */
+public final class ProcessInfo {
+  private final int id;
+  private final String name;
+
+  /**
+   * Constructs a ProcessInfo object belonging to a process.
+   * @param id the id of the process
+   * @param name the name of the process
+   */
+  public ProcessInfo(int id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  /**
+   * Get the id of the process.
+   * @return the id of the process
+   */
+  public int getId() {
+    return this.id;
+  }
+
+  /**
+   * Get the name of the process.
+   * @return the name of the process
+   */
+  public String getName() {
+    return this.name;
+  }
+}
